ca148711251e1eda54fd2fb52631088b236052cf
ElenaMegia <emegican@myuax.com>
2022-12-06 19:16:15 +0100

    añadido mapa y datos

--- app/src/main/java/com/example/prueba_programacion_android_23/Farmacia.java
+++ app/src/main/java/com/example/prueba_programacion_android_23/Farmacia.java
@@ -5,11 +5,19 @@ public class Farmacia {
     private long id;
     private String nombre;
     private String telefono;
+    private double latitud;
+    private double longitud;
 
-    public Farmacia(long id, String nombre, String telefono) {
+    public Farmacia(long id, String nombre, String telefono, double latitud, double longitud) {
         this.id = id;
         this.nombre = nombre;
         this.telefono = telefono;
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public Farmacia() {
+
     }
 
     public long getId() {
@@ -36,12 +44,30 @@ public class Farmacia {
         this.telefono = telefono;
     }
 
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
     @Override
     public String toString() {
         return "Farmacia{" +
                 "id=" + id +
                 ", nombre='" + nombre + '\'' +
                 ", telefono='" + telefono + '\'' +
+                ", latitud=" + latitud +
+                ", longitud=" + longitud +
                 '}';
     }
 }
--- app/src/main/java/com/example/prueba_programacion_android_23/MapsActivity.java
+++ app/src/main/java/com/example/prueba_programacion_android_23/MapsActivity.java
@@ -2,10 +2,12 @@ package com.example.prueba_programacion_android_23;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -16,6 +18,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    String nombre;
+    double latitud, longitud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,24 +32,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Intent intent = getIntent();
+        nombre = intent.getStringExtra("nombre");
+        latitud = intent.getDoubleExtra("latitud", 0);
+        longitud = intent.getDoubleExtra("longitud", 0);
+
+        setContentView(R.layout.activity_maps);
+        MapView mapa = (MapView) findViewById(R.id.map);
+        mapa.onCreate(savedInstanceState);
+        mapa.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng zaragoza = new LatLng(41.6560600, -0.8773400);
+        mMap.addMarker(new MarkerOptions().position(zaragoza).title("Marker in Zaragoza"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(zaragoza));
     }
 }
